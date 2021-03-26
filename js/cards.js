5e59b139e8beb87fe3fe9a795ae018c56bb3bb47
marwin marte <marwinmarte@gmail.com>
2021-03-25 22:52:42 -0400

    hola

--- js/cards.js
+++ js/cards.js
@@ -4,7 +4,9 @@
 
 
 // ------------------------------------------------------------------- //
-    let container = document.getElementById("cardContainer");
+    // let container = document.getElementById("cardContainer");
+    // console.log(container)
+
 
     fetch("https://authentic-ether-303815.uc.r.appspot.com/business/showVerified", {
       method: "GET",
@@ -13,17 +15,48 @@
       .then((data) => {
         console.log(data);
         // const card = document.createElement('div');
-        for(let i = 0; i < data.length; i++) { container.innerHTML += `
-        <div class="card">
-        <img src="images/evento2.jpg">
-        <div class="info">
-            <p class="categoria clase">${data[i].TypeOf}</p>
-            <p class="titulo">${data[i].Name}</p>
-            <p class="precio fas fa-map-marker-alt">  ${data[i].Province}</p>
-        </div>
-    </div>
-        `;}
-    
+        for(let i = 0; i < data.length; i++) { 
+          let categoria = data[i].TypeOf;
+          
+          if(container = document.getElementById("cardContainer")) {
+
+            if(categoria == "Restaurante"){
+                container.innerHTML += `
+                <div class="card">
+                <img src="images/evento2.jpg">
+                <div class="info">
+                    <p class="categoria concierto">${categoria}</p>
+                    <p class="titulo">${data[i].Name}</p>
+                    <p class="precio fas fa-map-marker-alt">  ${data[i].Province}</p>
+                </div>
+             </div>  `;
+            }
+
+          }
+          else if(container = document.getElementById("cardContainer2")) {
+      
+            if (categoria == "Hotel"){ 
+                  container.innerHTML += `
+                  <div class="card">
+                  <img src="images/evento2.jpg">
+                  <div class="info">
+                      <p class="categoria clase">${categoria}</p>
+                      <p class="titulo">${data[i].Name}</p>
+                      <p class="precio fas fa-map-marker-alt">  ${data[i].Province}</p>
+                  </div>
+              </div> `;
+            }
+
+          }
+        
+        
+         
+      
+      
+      } 
       });
+
+      
+      
   
   //
